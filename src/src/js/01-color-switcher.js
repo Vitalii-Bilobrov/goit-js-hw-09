de2edf4b54c7ea3815fb87e9de759e738d4d5d58
Vitalii-Bilobrov <106316488+Vitalii-Bilobrov@users.noreply.github.com>
2022-11-13 10:08:50 +0200

    commit_1

--- src/src/js/01-color-switcher.js
+++ src/src/js/01-color-switcher.js
@@ -3,9 +3,11 @@ const stopBtn = document.querySelector('[data-stop]');
 
 startBtn.addEventListener('click', onStartClick);
 function onStartClick() {
-  timerId = setInterval(getRandomHexColor, 1000);
+  timerId = setInterval(() => {
+    body.style.bacgroundColor = getRandomHexColor();
+  }, 1000);
 }
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-body.style.bacgroundColor = getRandomHexColor();
