82dfbef9ce9302d421738cf190127b53979b5f26
Vitalii-Bilobrov <106316488+Vitalii-Bilobrov@users.noreply.github.com>
2022-11-15 09:58:43 +0200

    Commit_4

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -9,7 +9,50 @@ const dataDays = document.querySelector('span[data-days]');
 const dataHours = document.querySelector('span[data-hours]');
 const dataMinutes = document.querySelector('span[data-minutes]');
 const dataSeconds = document.querySelector('span[data-seconds]');
-const x = new flatpickr(input);
+startBtn.disabled = true;
+let timerId = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < new Date()) {
+      alert('Please choose a date in the future');
+      return;
+    }
+    startBtn.disabled = false;
+
+    function counts() {
+      let now = new Date();
+      localStorage.setItem('selectedDates', selectedDates[0]);
+
+      let ms = localStorage.getItem('selectedDates');
+      if (!ms) return;
+      let diff = new Date(ms) - now;
+      const { days, hours, minutes, seconds } = convertMs(diff);
+      console.log(days, hours, minutes, seconds);
+      dataSeconds.textContent = seconds;
+      dataMinutes.textContent = minutes;
+      dataHours.textContent = hours;
+      dataDays.textContent = days;
+      if (days === 0 && hours === 0 && minutes === 0 && seconds === 0) {
+        clearInterval(timerId);
+        timerId = null;
+        startBtn.disabled = true;
+      }
+    }
+    function onStartBtnClick() {
+      if (timerId) {
+        clearInterval(timerId);
+      }
+      counts();
+      timerId = setInterval(counts, 1000);
+    }
+    startBtn.addEventListener('click', onStartBtnClick);
+  },
+};
 
 timerBox.style.display = 'flex';
 timerBox.style.gap = '20px';
@@ -19,43 +62,24 @@ fieldBoxes.forEach(element => {
   element.firstElementChild.style.fontSize = '25px';
   element.lastElementChild.style.fontSize = '15px';
 });
-function counts() {
-  let date = new Date('2022-11-15 00:00:00');
-  let now = new Date();
-  ms = date - now;
 
-  function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-    return { days, hours, minutes, seconds };
-    dataSeconds = seconds;
-    dataMinutes = minutes;
-    dataHours = hours;
-    dataDays = days;
-  }
+  return { days, hours, minutes, seconds };
 }
-counts();
-startBtn.addEventListener('click', setInterval(counts, 1000));
 
-// const options = {
-//   enableTime: true,
-//   time_24hr: true,
-//   defaultDate: new Date(),
-//   minuteIncrement: 1,
-//   onClose(selectedDates) {
-//     console.log(selectedDates[0]);
-//   },
-// };
+flatpickr(input, options);
